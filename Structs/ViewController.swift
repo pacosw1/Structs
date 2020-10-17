da174d788f4a2be938dc282b66d99ba062eb988a
Paco <pacoswilliams@gmail.com>
2020-10-17 15:30:32 -0500

    Initial Commit

---
+++ Structs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Structs
+//
+//  Created by paco williams on 10/17/20.
+//  Copyright © 2020 Francisco Sainz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
