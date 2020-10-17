130c74d50e0df747e2374df703e4143e0d10893d
Paco <pacoswilliams@gmail.com>
2020-10-17 18:08:41 -0500

    started implementing topics view list

---
+++ Structs/TopicCell.swift
@@ -0,0 +1,17 @@
+//
+//  TopicCell.swift
+//  Structs
+//
+//  Created by paco williams on 10/17/20.
+//  Copyright © 2020 Francisco Sainz. All rights reserved.
+//
+
+import UIKit
+
+class TopicCell: UITableViewCell {
+
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Locked: UIImageView!
+    
+    
+}
