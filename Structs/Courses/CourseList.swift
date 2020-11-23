1987b41266047a6e2cd7c770acdc3082d87684fa
AlePestana <marialepestana@gmail.com>
2020-11-22 23:43:54 -0500

    Modified title style

--- Structs/Courses/CourseList.swift
+++ Structs/Courses/CourseList.swift
@@ -11,29 +11,47 @@ import SwiftUI
 struct CourseList: View {
     
     @State var structs: [Struct]
+    @State var bigTitle: String = "STRUCTS"
+    @State var bigTitleColor: Color = Color.gray
     
     var body: some View {
         
+        VStack {
+            Button(action: {
+                print("Credits button was tapped")
+            }) {
+                Image(systemName: "info.circle.fill").foregroundColor(Color.black)
+            }.frame(width: UIScreen.main.bounds.width, height: 20, alignment: .trailing).padding(.trailing, 50.0)
+            
+            Text(bigTitle).fontWeight(.bold).font(.system(size: 22)).padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .background(bigTitleColor == Color.gray ? Color.white : bigTitleColor)
+                .foregroundColor(bigTitleColor == Color.gray ? Color.black : Color.white)
+                .cornerRadius(3)
+                .frame(width: UIScreen.main.bounds.width, height: 40, alignment: .center)
+           
+                Spacer()
+                
         NavigationView {
+            
             List {
                 ForEach(0..<structs.count) {
                     i in
-                    NavigationLink(destination: CourseHome(data: $structs[i], structIndex: i)) {
+                    NavigationLink(destination: CourseHome(data: $structs[i], bigTitle: $bigTitle, bigTitleColor: $bigTitleColor, structIndex: i)) {
                         CourseRow(data: $structs[i], structIndex: i)
+                        
                     }
+                    .listRowBackground(Color.white)
                 }
                 
-            }
-            .navigationBarItems(trailing: Button(action: {
-                print("Edit button was tapped")
-            }) {
-                Image(systemName: "info.circle.fill").foregroundColor(Color.black)
-            })
-            .navigationBarTitle(Text("STRUCTS"))
-            
+            }.navigationBarHidden(true)
             .onAppear(perform: {
                 structData = loadJSON("data.json")
             })
+            
+        }
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
         }
         
     }
